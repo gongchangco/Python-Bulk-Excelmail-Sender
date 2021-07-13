48d6a796169c2111c753a5fa3677dedcdea6e557
Grace <gongchie@gmail.com>
2021-07-13 11:43:50 -0400

    Update sendBulkEmails.py
    Add build_email and Items class

--- sendBulkEmails.py
+++ sendBulkEmails.py
@@ -1,5 +1,10 @@
 import openpyxl
+import smtplib
 import pandas as pd
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from bs4 import BeautifulSoup
 
 # SMTP information
 email_user = "########" # Add your SMTP user info
@@ -11,6 +16,47 @@ filename = ".xslx file location here"
 # Get active sheet
 wb = openpyxl.load_workbook(filename)
 ws = wb.active
+
+class Items:
+  def __init__(self, item_code, item_descript, order_qty):
+    self.item_code = item_code.split(',')
+    self.item_descript = item_descript.split(';')
+    self.order_qty = order_qty.split(',')
+    
+    self.item_list = []
+    
+    for i in range(0, len(self.item_code)):
+      temp = '''
+        <tr class="item">
+          <td class="add-info"><p>'''+ self.item_code[i] +'''</p></td>
+          <td class="td-wide"><p>'''+ self.item_descript[i] +'''</p></td>
+          <td class="add-info align-center"><p>'''+ self.order_qty[i] +'''</p></td>
+        </tr>
+      '''
+      self.item_list.append(temp)
+  
+def build_email(ordnum, ordate, ship_name, ship_addr1, ship_addr2, ship_addr3, ship_city, ship_state, ship_zipcode, c):
+  # Create email body
+  html = '''\
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <style type="text/css">
+          </style>
+        </head>
+        <body>
+          <h1 style="text-align: center;">Order Status</h1>
+        </body>
+      </html>
+  '''
+  
+  # Using BeautifulSoup to get rid of None type when cell value under ship addresses are blank
+  soup = BeautifulSoup(html, 'html.parser')
+  
+  return soup.prettify()
+  
 
 def send_email(user, password, sent_from, to, msg):
   # Enter SMTP credentials
